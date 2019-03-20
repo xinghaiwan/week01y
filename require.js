779390f07b81974ccc950f11cd0a5e1b27510f9b
liyueyang <l13717920694@163.com>
2019-03-20 08:57:12 +0800

    require

---
+++ require.js
@@ -0,0 +1,18 @@
+require(['a.js'], function() {
+
+});
+define([
+    'require',
+    'a'
+], function(require, factory) {
+    'use strict';
+
+});
+
+function require(arr, fn) {
+    let arr = [];
+}
+
+function define(arr) {
+    return arr;
+}
